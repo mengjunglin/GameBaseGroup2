8a9298c4b0ea88dc22fc1dce59c465ac00760b68
deepikak02 <dkrish15@asu.edu>
2016-11-06 02:28:16 -0700

    TG-35 #closed

---
+++ vectorball/Assets/1Assets/Scripts/ChooseOptionsManagerScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChooseOptionsManagerScript : MonoBehaviour {
+
+
+	//Toggle Game Objects
+	public Toggle isOptionA;
+	public Toggle isOptionB;
+	public Toggle isOptionC;
+	public Toggle isOptionD;
+
+	//Check which option is active
+	public char ActiveOption(){
+		//Set the chosen option as A or B or C or D below
+		char option = ' ';
+		if (isOptionA.isOn)
+			option = 'A';
+		else if (isOptionB.isOn)
+			option = 'B';
+		else if (isOptionC.isOn)
+			option = 'C';
+		else if (isOptionD.isOn)
+			option = 'D';
+		return option;
+	}
+
+	public void OnSubmit(){
+		//Check the selected option with correct option
+		Debug.Log("User chose "+ ActiveOption());
+		//If ActiveOption() == CorrectOptionForQuestion() call IfCorrectOption()
+		//Maintain TextAsset with q and a. 
+		//Script to make TextAsset values into array. CorrectOptionForQuestion(index) return ans(index)
+		//Can use same for generating question and display on canvas
+	}
+
+	public void IfCorrectOption(){
+		//Move ball and load next question
+	}
+
+	public void IfIncorrectOption(){
+		//Hint Popup. Maintain hint also in TextAsset
+	}
+		
+}
