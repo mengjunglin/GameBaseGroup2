3e8a70e5a170c7a6106f1516cacfb228ce9b0872
deepikak02 <dkrish15@asu.edu>
2016-11-06 19:45:08 -0700

    TG-46 #closed

---
+++ vectorball/Assets/1Assets/Scripts/SampleQuestionAnswerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SampleQuestionAnswerScript : MonoBehaviour {
+	public static string CorrectOptionForQuestion(int level){
+		//return correct answer(level) from TextAsset
+		return "A";
+	}
+	public static string GetQuestion(int level){
+		//Function will read the questions - Done by Hussain
+		return "Question 2";
+	}
+	public static string[] GetOptions(int level){
+		//Return the options read from TextAsset
+		return new string[]{"Option2A","Option2B","Option2C","Option2D"};
+	}
+}
