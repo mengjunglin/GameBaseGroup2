9d71332b6804f29d6919aa782c014a79825cf825
Alankrit Shah <ashah28@asu.edu>
2016-11-09 05:47:39 -0700

    Added function to set up grid and return the absolute position of a player at X,Y

--- vectorball/Assets/1Assets/Scripts/PlayerGridScript.cs
+++ vectorball/Assets/1Assets/Scripts/PlayerGridScript.cs
@@ -1,33 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 public enum Team { Home, Opponent}
 
 public class PlayerGridScript : MonoBehaviour {
 
     [SerializeField]
-    private PlayerScript[] playerTransforms;
+    private PlayerScript[] playerScripts;
     [SerializeField]
     private GameObject denseGridHolderL, denseGridHolderR;
 
-    public bool isDense;
+    public bool isDense { private set; get; }
 
 	// Use this for initialization
 	void Start () {
-        UpdatePlayerGrid();
-        playerTransforms = GetComponentsInChildren<PlayerScript>();
+        UpdatePlayerGrid(true);
+        playerScripts = GetComponentsInChildren<PlayerScript>();
 	}
 
-    public void UpdatePlayerGrid()
+    public void UpdatePlayerGrid(bool isDense)
     {
+        this.isDense = isDense;
+
         denseGridHolderL.SetActive(isDense);
         denseGridHolderR.SetActive(isDense);
     }
 	
 	public Vector3 GetAbsolutePosition(int x, int y)
     {
-        //PlayerScript selected = ;
+        print("Input x: " + x + " y:" + y);
+        if (!isDense)
+        {
+            if (y > 2)
+                y += 2;
+            else if (y > 1)
+                y++;
+        }
+
+        print("Processed x: " + x + " y:" + y);
+        PlayerScript selected = null;
+        Match positionIndices;
+        string[] xy = null;
+
+        foreach (PlayerScript ps in playerScripts)
+        {
+            positionIndices = Regex.Match(ps.name, "(?<=\\[).+?(?=\\])");
+            //if match not found throw error
+            xy = positionIndices.Value.Split(',');
+            if (xy[0] == x.ToString() && xy[1] == y.ToString())
+            {
+                selected = ps;
+                print(ps.name + " => x: " + xy[0] + " y:" + xy[1]);
+
+                return selected.transform.position;
+            }
+        }
+        print(xy + "  x: " + xy[0] + " y:" + xy[1]);
+        print("Not Found. ERROR");
         return Vector3.zero;
     }
 }
