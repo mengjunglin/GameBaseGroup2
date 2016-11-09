2db689d982be9dc09bd2ab40e1fd63fc446b1eda
Alankrit Shah <ashah28@asu.edu>
2016-11-09 05:03:21 -0700

    Created grid. Get grid position() WIP.

---
+++ vectorball/Assets/1Assets/Scripts/PlayerGridScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public enum Team { Home, Opponent}
+
+public class PlayerGridScript : MonoBehaviour {
+
+    [SerializeField]
+    private PlayerScript[] playerTransforms;
+    [SerializeField]
+    private GameObject denseGridHolderL, denseGridHolderR;
+
+    public bool isDense;
+
+	// Use this for initialization
+	void Start () {
+        UpdatePlayerGrid();
+        playerTransforms = GetComponentsInChildren<PlayerScript>();
+	}
+
+    public void UpdatePlayerGrid()
+    {
+        denseGridHolderL.SetActive(isDense);
+        denseGridHolderR.SetActive(isDense);
+    }
+	
+	public Vector3 GetAbsolutePosition(int x, int y)
+    {
+        //PlayerScript selected = ;
+        return Vector3.zero;
+    }
+}
