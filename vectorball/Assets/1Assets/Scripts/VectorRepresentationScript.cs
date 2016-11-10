ef85cd75ad184316771ad16ede85af59b57e819d
Digant Jagtap <djagtap1@asu.edu>
2016-11-10 01:02:30 -0700

    Added Vector Representation

--- vectorball/Assets/1Assets/Scripts/VectorRepresentationScript.cs
+++ vectorball/Assets/1Assets/Scripts/VectorRepresentationScript.cs
@@ -1,12 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class VectorRepresentationScript : MonoBehaviour {
 
+
+	public GameObject dialogueCanvasObj;
+
 	// Use this for initialization
 	void Start () {
-	
 
+		GameObject dialogueCanvasObj = GameObject.FindGameObjectWithTag ("DialogueCanvas");
+		//dialogueCanvasObj.SetActive(false);
+		convertResultToVector (true,0,"2,3");
 
 	}
 	
@@ -15,11 +21,33 @@ public class VectorRepresentationScript : MonoBehaviour {
 	
 	}
 
-	void convertResultToVector(int level, string result){
+	public void convertResultToVector (bool status, int level, string result)
+	{
+		// Format of result : 2,3
+		string dialogueMessage = "";
+
+		//GameObject dialogueCanvasObj = GameObject.FindGameObjectWithTag ("DialogueCanvas");
+		dialogueCanvasObj.SetActive(true);
+		GameObject dialogueObj = GameObject.FindGameObjectWithTag ("Dialogue");
+
+
+		GameObject resultObj = GameObject.FindGameObjectWithTag ("Result");
+		GameObject resultBoxObj = GameObject.FindGameObjectWithTag ("Result");
+		if (!status) {
+			dialogueMessage = "Whoops! You gave the ball to opponent. Try again!";
+			//resultObj.GetComponent<Text> ().text.
+		} else {
+			dialogueMessage = "Hurray! That was an awesome pass. In vector world it means";
+		}
+		dialogueObj.GetComponent<Text> ().text = dialogueMessage;
 		if (level == 0) {
-			
+			// addleftBrk(highScoreText,result);
+			string[] coordinates = result.Split (',');
+				
+			resultObj.GetComponent<Text> ().text = string.Format ("{0} \n{1}", coordinates [0], coordinates [1]);
+			// Text rightBrk = highScoreText.gameObject.transform.parent.gameObject.AddComponent<Text> ();
+			// rightBrk.text = "]";
+			//Debug.Log (string.Format("[{0} \n {1}] = {2}", a, b, c));
 		}
 	}
-
-
 }
