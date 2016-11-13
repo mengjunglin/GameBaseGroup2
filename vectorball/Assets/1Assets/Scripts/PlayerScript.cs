c104fb0ae33af3af211e4596602bbe182b7aadd1
Alankrit Shah <ashah28@asu.edu>
2016-11-13 16:00:20 -0700

    Added kick trigger and configured animator states to work with.

--- vectorball/Assets/1Assets/Scripts/PlayerScript.cs
+++ vectorball/Assets/1Assets/Scripts/PlayerScript.cs
@@ -4,14 +4,22 @@ using System.Collections;
 public class PlayerScript : MonoBehaviour {
 
     public bool isOpponent;
+    public Animator anim;
 
     void Awake()
     {
         isOpponent = (name.Contains("PositionB"));
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+            PlayKickAnimation();
     }
 
     public void PlayKickAnimation()
     {
-
+        anim.SetTrigger("Kick");
     }
 }
