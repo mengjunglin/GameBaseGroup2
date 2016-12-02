9b6bbb405e205df0ca21e1cdf05474a583601951
Alankrit Shah <ashah28@asu.edu>
2016-12-02 06:19:41 -0700

    Added character animation in home screen.

---
+++ vectorball/Assets/1Assets/Scripts/AnimationCycler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Quick and dirty implementation
+/// </summary>
+public class AnimationCycler : MonoBehaviour {
+
+
+    PlayFaceAnimations pfScript;
+
+    [SerializeField]
+    Eyes_Expressions[] anims;
+
+    [SerializeField]
+    float[] duration;
+
+    [SerializeField]
+    int index;
+
+	// Use this for initialization
+	void Start () {
+        pfScript = GetComponent<PlayFaceAnimations>();
+        StartCoroutine(CycleAnims());
+	}
+	
+	IEnumerator CycleAnims()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(duration[index]);
+
+            index++;
+            index %= anims.Length;
+
+            pfScript.Eye = anims[index];
+        }
+    }
+
+}
